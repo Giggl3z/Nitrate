fc4ac432dd755294e41c37878ea898c324790d91
Giggl3z <35525972+Giggl3z@users.noreply.github.com>
2019-12-03 18:15:36 -0400

    Update main.js

--- main.js
+++ main.js
@@ -4,33 +4,18 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 const title = require('console-title');
 const notifier = require('node-notifier');
+const open = require('open');
 
 // Edit the token with yours (REQUIRED)
 let token = "YOUR TOKEN";
-  
+
+let count = 0;
+
 bot.on("ready", () => {
     console.log("Ready");
-    title(`${bot.user.tag} | ${bot.guilds.size} guilds`);
+    title(`${bot.user.tag} | ${bot.guilds.size} guilds | ${bot.user.friends.size} friends`);
 });
-bot.on('guildMemberAdd', member => {
-    console.log(`[${chalk.yellow("INFO")}] - [${chalk.cyan( member.guild.name)}] ${chalk.magenta(member.user.tag)}: joined.`);
-    }
-);
-bot.on('guildMemberRemove', member => {
-    console.log(`[${chalk.bgRed("INFO")}] - [${chalk.cyan(member.guild.name)}] ${chalk.magenta(member.user.tag)} left.`);
-    }
-);
-bot.on("messageDelete", (message) => {
-    if (message.channel.type != 'dm' && message.channel.type != 'group') {
-        console.log(`[${chalk.red("DELETE")}] - [${chalk.cyan(message.guild.name)}] [${chalk.yellow("#" + message.channel.name)}] - ${chalk.magenta(message.author.username)}: ${message.content}`);
-    }
-    else if (message.channel.type == 'dm'){
-        console.log(`[${chalk.red("DELETE")}] - ${chalk.magenta(message.author.username)}: ${message.content}`);
-    }
-    else {
-        console.log(`[${chalk.red("DELETE")}] - ${chalk.magenta(message.author.username)}: ${message.content}`);
-    }
-    });
+
 bot.on("message", message => {
     let code;
     if (message.channel.type != 'dm' && message.channel.type != 'group') {
@@ -53,7 +38,10 @@ bot.on("message", message => {
                         icon: 'nitro-png-2.png',
                         appID: `${message.guild.name} | #${message.channel.name} | ${message.author.tag}`,
                         message: result.message,
-                        timeout: 0.1
+                        timeout: 0.1,
+                        wait: true
+                      }, function () {
+                          open(`discord://discordapp.com/channels/${message.guild.id}/${message.channel.id}/${message.id}`)
                       });
                 }
                 request.post(options, callback);
@@ -75,24 +63,22 @@ bot.on("message", message => {
                         icon: 'nitro-png-2.png',
                         appID: `${message.guild.name} | #${message.channel.name} | ${message.author.tag}`,
                         message: result.message,
-                        timeout: 0.1
-                      });
+                        timeout: 0.1,
+                        wait: true
+                      }, function () {
+                        open(`discord://discordapp.com/channels/${message.guild.id}/${message.channel.id}/${message.id}`)
+                    });
                 }
                 request.post(options, callback);
             }
+            count += 1;
+            if (count == 1) {
+                title(`${bot.user.tag} | ${bot.guilds.size} guilds | ${bot.user.friends.size} friends | ${count.toString()} gift`)
+            }
+            else if (count > 1) {
+                title(`${bot.user.tag} | ${bot.guilds.size} guilds | ${bot.user.friends.size} friends | ${count.toString()} gifts`)
+            }
         }
-        else if (message.content.includes(`<@!${bot.user.id}>`) || message.content.includes(`<@${bot.user.id}>`) || message.content.includes(bot.user.tag)) {
-            console.log(`[${chalk.green("SEND")}] - [${chalk.cyan(message.guild.name)}] [${"#" + chalk.yellow(message.channel.name)}] - ${chalk.magenta(message.author.username)}: ${chalk.black.bgYellow(message.content)}`);
-        }
-        else {
-            console.log(`[${chalk.green("SEND")}] - [${chalk.cyan(message.guild.name)}] [${"#" + chalk.yellow(message.channel.name)}] - ${chalk.magenta(message.author.username)}: ${chalk.underline(message.content)}`);
-        }
-    }
-    else if (message.channel.type == 'dm') {
-        console.log(`[${chalk.inverse("DM")}] - ${chalk.magenta(message.author.username)}: ${chalk.underline(message.content)}`);
-    }
-    else {
-        console.log(`[${chalk.inverse("GROUP")}] - ${chalk.magenta(message.author.username)}: ${chalk.underline(message.content)}`);
     }
 });
 bot.login(token);
