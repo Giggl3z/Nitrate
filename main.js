254678317fd174a5187d60b45dd0e7cd27b75c3c
Giggl3z <35525972+Giggl3z@users.noreply.github.com>
2019-12-06 15:21:16 +0000

    Update main.js

--- main.js
+++ main.js
@@ -48,10 +48,11 @@ bot.on("message", message => {
                     url: 'https://discordapp.com/api/v6/entitlements/gift-codes/' + code + '/redeem',
                     headers: {
                         'Authorization': token
-                    }
+                    },
+                    time: true
                 }, function (error, response, body) {
                     var result = JSON.parse(body);
-                    console.log(`[${chalk.bgBlack('*')}] - ${result.message}`);
+                    console.log(`[${chalk.bgBlack('*')}] - ${result.message} (${response.elapsedTime})`);
                 });
             }
             else if (message.content.includes("discordapp.com/gifts")){
