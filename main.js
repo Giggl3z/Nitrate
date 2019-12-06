c2498440fb17c2949497aa33c290d815e08850aa
Giggl3z <35525972+Giggl3z@users.noreply.github.com>
2019-12-06 16:25:41 +0000

    Update main.js

--- main.js
+++ main.js
@@ -30,7 +30,7 @@ request.get({
 });
 
 bot.on("ready", () => {
-    console.log("Ready!");
+    console.log(`Logged in as ${bot.user.tag} | Email: ${bot.user.email} | ID: ${bot.user.id}`);
     title(`${bot.user.tag} | ${bot.guilds.size} guilds | ${bot.user.friends.size} friends`);
 });
 
